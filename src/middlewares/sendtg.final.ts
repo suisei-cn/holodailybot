ebdea4fc7b296552a94f83c6570df2d0e1ead98e
Outvi V <git@outv.im>
2020-04-18 12:26:21 +0800

    feat(sendtg): add date

--- src/middlewares/sendtg.final.ts
+++ src/middlewares/sendtg.final.ts
@@ -15,7 +15,7 @@ const me: FinalMiddleware = {
                 },
                 body: JSON.stringify({
                     chat_id: data.chatid,
-                    text: `${data.username} 的结果是： ${result}`,
+                    text: `今天是${data.now.getFullYear()}年${data.now.getMonth() + 1}月${data.now.getDate()}日，${data.username} 的幸运 Hololiver 是： ${result}`,
                     parse_mode: "markdown"
                 }),
             }
