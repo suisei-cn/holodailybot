c14e5fac498b74922d5bbb69c86e9023c322ac6a
Outvi V <git@outv.im>
2020-04-18 23:17:46 +0800

    feat(utils): Move getMMDD out

--- src/middlewares/birthday.change.ts
+++ src/middlewares/birthday.change.ts
@@ -1,16 +1,5 @@
 import { ChangeMiddleware } from "../types";
-
-function getMMDD(d: Date) {
-    let month = String(d.getMonth() + 1),
-        day = String(d.getDate());
-
-    if (month.length < 2)
-        month = '0' + month;
-    if (day.length < 2)
-        day = '0' + day;
-
-    return month + day;
-}
+import { getMMDD } from "./utils";
 
 const me: ChangeMiddleware = {
     type: "change",
---
+++ src/middlewares/utils.ts
@@ -0,0 +1,11 @@
+export function getMMDD(d: Date) {
+    let month = String(d.getMonth() + 1),
+        day = String(d.getDate());
+
+    if (month.length < 2)
+        month = '0' + month;
+    if (day.length < 2)
+        day = '0' + day;
+
+    return month + day;
+}
