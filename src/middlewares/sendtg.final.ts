7b69cddede0a18d06d2f4dc6fac9d1be3af052f4
Outvi V <git@outv.im>
2020-04-18 12:34:42 +0800

    feat: add saying/meme

--- src/middlewares/sendtg.final.ts
+++ src/middlewares/sendtg.final.ts
@@ -1,11 +1,29 @@
 import { FinalMiddleware } from "../types";
 const fetch = require("node-fetch");
 const BOT_KEY = process.env.TELEGRAM_BOT_KEY;
+import hololiverInfo from "../hololiverInfo";
 
 const me: FinalMiddleware = {
     type: "final",
     payload(result, data: any) {
         if (!data.valid) return;
+        let name = result[0];
+        let rnd = result[1];
+        // @ts-ignore: Element implicitly has an 'any' type
+        let extraInfo = hololiverInfo[name] || [];
+        let text = `今天是${data.now.getFullYear()}年${data.now.getMonth() + 1}月${data.now.getDate()}日，${data.username} 的幸运 Hololiver 是： ${name}`;
+        if (extraInfo.length !== 0) {
+            let cookie = extraInfo[Math.floor(rnd * extraInfo.length)];
+            if (typeof cookie === "string") {
+                text += `\n今天的 Hololiver 语录：${cookie}`
+            } else if (typeof cookie === "object") {
+                switch (cookie.type) {
+                    case "urlimage": {
+                        text += `\n今天的 Hololiver 梗图：[\u200b](${cookie.payload})`
+                    }
+                }
+            }
+        }
         fetch(
             `https://api.telegram.org/bot${BOT_KEY}/sendMessage`,
             {
@@ -15,7 +33,7 @@ const me: FinalMiddleware = {
                 },
                 body: JSON.stringify({
                     chat_id: data.chatid,
-                    text: `今天是${data.now.getFullYear()}年${data.now.getMonth() + 1}月${data.now.getDate()}日，${data.username} 的幸运 Hololiver 是： ${result[0]}`,
+                    text,
                     parse_mode: "markdown"
                 }),
             }
