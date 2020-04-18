ffb9505378fc9ae785096b8cd56b79a18a6781a6
Outvi V <19144373+outloudvi@users.noreply.github.com>
2020-04-18 23:26:10 +0800

    docs: TELEGRAM_ADMINCHAT_ID

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,10 @@ module.exports = {
     max_memory_restart: '100M',
     env: {
       PORT: 3000,
-      TELEGRAM_BOT_KEY: ""
+      TELEGRAM_BOT_KEY: "",
+      // If it's set, at the first gacha of every day, the gacha statistics for the last day
+      //   will be sent to this Telegram chat.
+      TELEGRAM_ADMINCHAT_ID: -1
     }
   }]
 };
