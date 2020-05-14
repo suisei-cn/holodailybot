3c56926a389346e4e1a80dc03d821a79f572df2f
Outvi V <git@outv.im>
2020-05-14 20:14:36 +0800

    feat(birthday): Birthday for everyone!

--- src/middlewares/birthday.change.ts
+++ src/middlewares/birthday.change.ts
@@ -5,89 +5,93 @@ const me: ChangeMiddleware = {
     type: "change",
     payload(selection, data: any) {
         let md = getMMDD(data.now);
+        let result: [string, number] | undefined;
         switch (md) {
             case "0112": {
-                return ["兔田佩克拉", Math.random()];
+                result = ["兔田佩克拉", Math.random()];
             }
             case "0122": {
-                return ["润羽露西娅", Math.random()];
+                result = ["润羽露西娅", Math.random()];
             }
             case "0214": {
-                return ["癒月巧可", Math.random()];
+                result = ["癒月巧可", Math.random()];
             }
             case "0222": {
-                return ["猫又小粥", Math.random()];
+                result = ["猫又小粥", Math.random()];
             }
             case "0305": {
-                return ["樱巫女", Math.random()];
+                result = ["樱巫女", Math.random()];
             }
             case "0322": {
-                return ["星街彗星", Math.random()];
+                result = ["星街彗星", Math.random()];
             }
             case "0402": {
-                return ["不知火芙蕾雅", Math.random()];
+                result = ["不知火芙蕾雅", Math.random()];
             }
             case "0422": {
-                return ["天音彼方", Math.random()];
+                result = ["天音彼方", Math.random()];
             }
             case "0515": {
-                return ["时乃空", Math.random(), {
-                    prefix: "时乃空生日快乐！"
-                }];
+                result = ["时乃空", Math.random()];
             }
             case "0523": {
-                return ["萝卜子", Math.random()];
+                result = ["萝卜子", Math.random()];
             }
             case "0606": {
-                return ["角卷绵芽", Math.random()];
+                result = ["角卷绵芽", Math.random()];
             }
             case "0617": {
-                return ["桐生可可", Math.random()];
+                result = ["桐生可可", Math.random()];
             }
             case "0702": {
-                return ["大空昴", Math.random()];
+                result = ["大空昴", Math.random()];
             }
             case "0722": {
-                return ["夏色祭", Math.random()];
+                result = ["夏色祭", Math.random()];
             }
             case "0730": {
-                return ["宝钟玛琳", Math.random()];
+                result = ["宝钟玛琳", Math.random()];
             }
             case "0808": {
-                return ["常暗永远", Math.random()];
+                result = ["常暗永远", Math.random()];
             }
             case "0810": {
-                return ["赤井心", Math.random()];
+                result = ["赤井心", Math.random()];
             }
             case "0820": {
-                return ["大神澪", Math.random()];
+                result = ["大神澪", Math.random()];
             }
             case "1001": {
-                return ["戌神沁音", Math.random()];
+                result = ["戌神沁音", Math.random()];
             }
             case "1005": {
-                return ["白上吹雪", Math.random()];
+                result = ["白上吹雪", Math.random()];
             }
             case "1010": {
-                return ["姬森璐娜", Math.random()];
+                result = ["姬森璐娜", Math.random()];
             }
             case "1031": {
-                return ["夜空梅露", Math.random()];
+                result = ["夜空梅露", Math.random()];
             }
             case "1124": {
-                return ["白银诺艾尔", Math.random()];
+                result = ["白银诺艾尔", Math.random()];
             }
             case "1201": {
-                return ["湊阿库娅", Math.random()];
+                result = ["湊阿库娅", Math.random()];
             }
             case "1208": {
-                return ["紫咲诗音", Math.random()];
+                result = ["紫咲诗音", Math.random()];
             }
             case "1213": {
-                return ["百鬼绫目", Math.random()];
+                result = ["百鬼绫目", Math.random()];
             }
         }
 
+        if (result) {
+            return [...result, {
+                prefix: `${result[0]}生日快乐！`
+            }]
+        }
         return selection;
     }
 }
