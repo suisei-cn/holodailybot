7e99487609f9cb08a87e16acd544a87e9eebbf1f
Outvi V <git@outv.im>
2020-06-21 21:50:56 +0800

    feat(getHumanReadableDate): remove spaces

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export function getHumanReadableDate(date: Date) {
-    return `${date.getFullYear()} 年${date.getMonth() + 1} 月${date.getDate()} 日`;
+    return `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`;
 }
 
 export function extractQuery(body: any): string | undefined {
