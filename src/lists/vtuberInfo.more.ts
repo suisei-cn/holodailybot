c8b99870380fd139f94178b812ef4d30d9208181
Outvi V <19144373+outloudvi@users.noreply.github.com>
2020-06-22 13:34:39 +0800

    feat(vtuberInfo.more): add Kana to list

--- src/lists/vtuberInfo.more.ts
+++ src/lists/vtuberInfo.more.ts
@@ -13,7 +13,8 @@ const exp: ItemPickList = {
       // https://t.bilibili.com/396130004857483240?tab=2
       payload: 'https://i0.hdslb.com/bfs/album/c23c249ef2aedfecf9aa683d4e27d8cc1209d2dd.jpg@518w_1e_1c.jpg',
     } as ItemPick
-  ]
+  ],
+  小雀香奈: []
 }
 
 export const info: { [key: string]: any } = {
