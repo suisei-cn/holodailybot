5af2d3f741ff6ce4b7e7c823f28898a17521d304
Outvi V <git@outv.im>
2020-09-20 01:47:45 +0800

    fix: body is in

--- src/specials.ts
+++ src/specials.ts
@@ -2,7 +2,7 @@ import { tgSendMessage } from './util.tg'
 
 import base64url from 'base64url'
 
-export default function act({ body }: { body: any }) {
+export default function act(body: any) {
   const message = body?.message?.text
   if (!message) return
   const start = message.replace(/\/start(@[a-zA-z0-9-]*)? */, '')
@@ -10,10 +10,9 @@ export default function act({ body }: { body: any }) {
   const split = data.split('|')
   if (split.length < 2) return
   const opr = split[0]
-  const target = split.slice(0).join('|')
+  const target = split.slice(1).join('|')
   switch (opr) {
     case 'R': {
-      console.log(`${opr} checking`)
       tgSendMessage(
         body.message.chat.id,
         `[报告毕业 VTuber] 「${target}」已经提交。`,
