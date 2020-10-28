98fca14d8e037f6edabe778b5a2ae9370103afb2
Outvi V <git@outv.im>
2020-10-28 15:49:32 +0800

    feat: add namemap and emoji for 星川サラ

--- src/lists/emoji.ts
+++ src/lists/emoji.ts
@@ -1,6 +1,9 @@
+// Note: names here shall correspond to the ones in extras.info.json (or vtuberInfo.*.ts).
+
 const emojis: { [key: string]: string } = {
   星街彗星: '☄️',
   夜空梅露: '🌟',
+  星川莎拉: '🌟',
 }
 
 export default emojis
